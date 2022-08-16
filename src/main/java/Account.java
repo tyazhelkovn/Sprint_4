5eb755d298fa928c4fa022115b4b39870b5b9472
Nikita Tyazhelkov <nikita.tyazhelkov@yandex.ru>
2022-08-16 20:41:50 +0300

    fix

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -7,18 +7,22 @@ public class Account {
     }
 
     public boolean checkNameToEmboss() {
-        if ((name.length() >= 3 && name.length() <= 19)
-                && !name.endsWith(" ")
-                && !name.startsWith(" ")) {
-            int count = 0;
-            for (int i = 0; i < name.length(); i++) {
-                if (name.charAt(i) == ' ') {
-                    count++;
+        try {
+            if ((name.length() >= 3 && name.length() <= 19)
+                    && !name.endsWith(" ")
+                    && !name.startsWith(" ")) {
+                int count = 0;
+                for (int i = 0; i < name.length(); i++) {
+                    if (name.charAt(i) == ' ') {
+                        count++;
+                    }
+                }
+                if (count == 1) {
+                    return true;
                 }
             }
-            if (count == 1) {
-                return true;
-            }
+        } catch (NullPointerException e) {
+            throw e;
         }
         return false;
     }
